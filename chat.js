2965409aa430df5a2b4310ac5539367450278547
saketrule <anonymoussaketjoshi@gmail.com>
2017-04-13 06:53:50 +0530

    Commit 2

---
+++ chat.js
@@ -0,0 +1,94 @@
+var instanse = false;
+var state;
+var mes;
+var file;
+var room;
+var nickname;
+
+function Chat () {
+	this.update = updateChat;
+    this.send = sendChat;
+	this.getState = getStateOfChat;
+	this.init = initializeChat;
+}
+
+//gets the state of the chat
+function getStateOfChat(){
+	if(!instanse){
+		 instanse = true;
+		 $.ajax({
+			   type: "POST",
+			   url: "process.php",
+			   data: {  
+			   			'function': 'getState',
+						'file': file,
+						'room': room
+						},
+			   dataType: "json",
+			
+			   success: function(data){
+				   state = data.state;
+				   instanse = false;
+			   },
+			});
+	}	 
+}
+
+function initializeChat(name,place){
+	nickname = name;
+	room = place;
+	state = 0;
+}
+
+//Updates the chat
+function updateChat(){
+	 if(!instanse){
+		 instanse = true;
+	     $.ajax({
+			   type: "POST",
+			   url: "process.php",
+			   data: {  
+			   			'function': 'update',
+						'state': state,
+						'file': file,
+						'room': room
+						},
+			   dataType: "json",
+			   success: function(data){
+				   if(data.text){
+						for (var i = 0; i < data.text.length; i++) {
+                            $('#chat-area').append($("<p>"+ data.text[i] +"</p>"));
+                        }								  
+						document.getElementById('chat-area').scrollTop = document.getElementById('chat-area').scrollHeight;
+				   }
+
+				   instanse = false;
+				   state = data.state;
+			   },
+			});
+	 }
+	 else {
+		 setTimeout(updateChat, 1500);
+	 }
+}
+
+//send the message
+function sendChat(message,name)
+{       
+    updateChat();
+     $.ajax({
+		   type: "POST",
+		   url: "process.php",
+		   data: {  
+		   			'function': 'send',
+					'message': message,
+					'nickname': name,
+					'room': room,
+					'file': file
+				 },
+		   dataType: "json",
+		   success: function(data){
+			   updateChat();
+		   },
+		});
+}
